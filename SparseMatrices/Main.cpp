0354a095858305723084271fe4d383380e4769ae
Konstantin Belousov <kombuchamp@gmail.com>
2019-06-06 01:54:37 +0300

    Change main demonstration func

--- SparseMatrices/Main.cpp
+++ SparseMatrices/Main.cpp
@@ -8,24 +8,34 @@ using namespace std;
 
 int main()
 {
-	LLSparseMatrix<int> mat0(2, 2);
-	LLSparseMatrix<int> mat1(2, 2);
+	LLSparseMatrix<int> mat0(3, 3);
+	LLSparseMatrix<int> mat1(3, 3);
 
-	mat0.SetElement(0, 0, 0);
+	mat0.SetElement(0, 0, 1);
 	mat0.SetElement(0, 1, 0);
-	mat0.SetElement(1, 0, 1);
-	mat0.SetElement(1, 1, 1);
+	mat0.SetElement(0, 2, 4);
+	mat0.SetElement(1, 0, 2);
+	mat0.SetElement(1, 1, 0);
+	mat0.SetElement(1, 2, 5);
+	mat0.SetElement(2, 0, 3);
+	mat0.SetElement(2, 1, 0);
+	mat0.SetElement(2, 2, 6);
 
-	mat1.SetElement(0, 0, 1);
-	mat1.SetElement(0, 1, 1);
+	mat1.SetElement(0, 0, 0);
+	mat1.SetElement(0, 1, 7);
+	mat1.SetElement(0, 2, 0);
 	mat1.SetElement(1, 0, 0);
-	mat1.SetElement(1, 1, 0);
+	mat1.SetElement(1, 1, 8);
+	mat1.SetElement(1, 2, 0);
+	mat1.SetElement(2, 0, 0);
+	mat1.SetElement(2, 1, 9);
+	mat1.SetElement(2, 2, 0);
 
 	OUTPUT("***MATRICES***");
 	std::cout << mat0 << "\n" << std::endl;
 	std::cout << mat1 << "\n" << std::endl;
 	OUTPUT("*STARTING PROCESS...*");
-	std::unique_ptr<LLSparseMatrix<int>> mat2(mat0 * mat1);
+	auto mat2 = mat0 * mat1;
 	std::cout << "DONE: " << std::endl;
 	std::cout << *mat2;
 
